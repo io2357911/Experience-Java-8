803c18994365046d5f13420632851e529c275d42
Oleg Ivanov <io2357911@yandex.ru>
2021-04-17 11:06:59 +0300

    Fix shouldGroupByAge

--- src/main/java/academy/elqoo/java8/stream/Stream8.java
+++ src/main/java/academy/elqoo/java8/stream/Stream8.java
@@ -5,8 +5,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.partitioningBy;
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 public class Stream8 {
 
@@ -86,7 +85,7 @@ public class Stream8 {
     }
 
     public static Map<Integer, List<User>> groupByAge(List<User> users) {
-        throw new UnsupportedOperationException();
+        return users.stream().collect(groupingBy(User::getAge));
     }
 
     public static Map<Boolean, Map<Integer, List<User>>> groupByGenderAndAge(List<User> users) {
